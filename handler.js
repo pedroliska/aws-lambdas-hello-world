f62668b7373d8bc6f7137346327399b1baecf020
Pedro Liska <pliska@1800contacts.com>
2022-07-14 17:08:28 -0600

    added commands txt

--- handler.js
+++ handler.js
@@ -1,12 +1,35 @@
 'use strict';
 
 module.exports.hello = async (event) => {
+  shared();
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: 'Hello from handler',
         input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+function shared() {
+  // common code here
+}
+
+
+module.exports.hellooo = async (event) => {
+  shared();
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Hello from handler.hello2'
       },
       null,
       2
