d03b83435ec45e1b6953e06ff633e892cd5199de
Pedro Liska <pliska@1800contacts.com>
2022-07-18 14:25:21 -0600

    We now have a total of 3 integration tests

--- 02-production-ready-serverless/tests/steps/when.js
+++ 02-production-ready-serverless/tests/steps/when.js
@@ -10,11 +10,21 @@ const viaHandler = async (event, functionName) => {
   if (response.body && contentType === 'application/json') {
     response.body = JSON.parse(response.body);
   }
-  return response
+  return response;
 }
 
-const we_invoke_get_index = () => viaHandler({}, 'get-index')
+const we_invoke_get_index = () => viaHandler({}, 'get-index');
+const we_invoke_get_restaurants = () => viaHandler({}, 'get-restaurants');
+const we_invoke_search_restaurants = theme => {
+  let event = { 
+    body: JSON.stringify({ theme })
+  }
+  return viaHandler(event, 'search-restaurants')
+};
+
 
 module.exports = {
-  we_invoke_get_index
+  we_invoke_get_index,
+  we_invoke_get_restaurants,
+  we_invoke_search_restaurants
 }
--- 02-production-ready-serverless/tests/test_cases/get-index.tests.js
+++ 02-production-ready-serverless/tests/test_cases/get-index.tests.js
@@ -1,7 +1,11 @@
 const cheerio = require('cheerio')
 const when = require('../steps/when')
+const { init } = require('../steps/init')
 
 describe(`When we invoke the GET / endpoint`, () => {
+
+  beforeAll(async () => await init())
+
   it(`Should return the index page with 8 restaurants`, async () => {
     const res = await when.we_invoke_get_index()
 
@@ -11,6 +15,6 @@ describe(`When we invoke the GET / endpoint`, () => {
 
     const $ = cheerio.load(res.body)
     const restaurants = $('.restaurant', '#restaurantsUl')
-    expect(restaurants.length).toEqual(8)
+    expect(restaurants.length).toEqual(7)
   })
 })
