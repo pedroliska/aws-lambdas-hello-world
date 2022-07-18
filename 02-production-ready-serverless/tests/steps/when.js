9aae6b1d73bbc3aa9a92d23b915f54440d6fb65a
Pedro Liska <pliska@1800contacts.com>
2022-07-18 16:12:36 -0600

    setup TEST_MODE env var

--- 02-production-ready-serverless/tests/steps/when.js
+++ 02-production-ready-serverless/tests/steps/when.js
@@ -1,3 +1,8 @@
+const aws4 = require('aws4')
+const URL = require('url')
+const http = require('axios')
+
+const mode = process.env.TEST_MODE
 const APP_ROOT = '../../'
 const _ = require('lodash')
 
@@ -13,7 +18,17 @@ const viaHandler = async (event, functionName) => {
   return response;
 }
 
-const we_invoke_get_index = () => viaHandler({}, 'get-index');
+const we_invoke_get_index = async () => {
+  switch (mode) {
+    case 'handler':
+      return await viaHandler({}, 'get-index')
+    case 'http':
+      return await viaHttp('', 'GET')
+    default:
+      throw new Error(`unsupported mode: ${mode}`)
+  }
+}
+
 const we_invoke_get_restaurants = () => viaHandler({}, 'get-restaurants');
 const we_invoke_search_restaurants = theme => {
   let event = { 
